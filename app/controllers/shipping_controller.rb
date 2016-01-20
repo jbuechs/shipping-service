a763e01d53dd1153f9a46051dafd7a88161d9d9d
Jennie Buechner <janina.buechner@gmail.com>
2016-01-20 14:03:37 -0800

    refactor fedex method to take params

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -1,22 +1,31 @@
 require 'active_shipping'
 
 class ShippingController < ApplicationController
-  def origin
-    ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", postal_code: "98102")
+  skip_before_filter :verify_authenticity_token
+  def location(loc_hash)
+    ActiveShipping::Location.new(loc_hash)
   end
 
-  def destination
-    ActiveShipping::Location.new(country: "US", state: "CA", city: "Los Angeles", postal_code: "90024")
-  end
+  # def destination
+  #   ActiveShipping::Location.new(country: "US", state: "CA", city: "Los Angeles", postal_code: "90024")
+  # end
 
-  def packages
+  def package(package_hash)
+    weight = package_hash["weight"]
+    width = package_hash["width"]
+    length = package_hash["length"]
+    height = package_hash["height"]
+    cylinder = package_hash["cylinder"]
     # package = Package.new(weight, [length, width, height], cylinder: cylinder)
-    return ActiveShipping::Package.new(100,[93,10],cylinder: true)
+    return ActiveShipping::Package.new(weight, [length, width, height], cylinder: cylinder)
   end
 
   def ups_rates
+    destination = location(params["destination"])
+    origin = location(params["origin"])
+    package = package(params["package"])
     ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_ACCESS_KEY'])
-    response = ups.find_rates(origin, destination, packages)
+    response = ups.find_rates(origin, destination, package)
     ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     render :json => ups_rates
   end
@@ -27,5 +36,5 @@ class ShippingController < ApplicationController
     fedex_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     render :json => fedex_rates
   end
-  
+
 end
